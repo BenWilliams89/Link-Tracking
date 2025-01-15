9ae71d70c46935e8c34841834b4df0bc3ba81379
Ben Williams <codebencode@icloud.com>
2025-01-15 12:28:32 +0000

    Add editing to blogs

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
 from .forms import PostForm
 
@@ -22,9 +22,15 @@ class ArticleDetailView(DetailView):
     template_name = 'article_details.html'
 
 
-class addPostView(CreateView):
+class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_posts.html'
     #fields = '__all__'
     #fileds = ('title', 'body') - this can bring through certain fields and not all
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'update_post.html'
+    #fields = ['title', 'body']
